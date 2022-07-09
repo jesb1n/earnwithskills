d90e9b7273652d30926585008f4ba1aa6896400a
Gokul P V <dwarakagokul@gmail.com>
2022-07-09 18:39:32 +0530

    commit 3

--- app/src/main/java/com/example/earnwithskills/login.java
+++ app/src/main/java/com/example/earnwithskills/login.java
@@ -3,14 +3,21 @@ package com.example.earnwithskills;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.earnwithskills.databinding.ActivityLoginBinding;
+import com.example.earnwithskills.databinding.ActivityLoginTypeBinding;
 
 public class login extends AppCompatActivity {
-
+    ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
+    public void ulogin(View view) {
+
+    }
 }

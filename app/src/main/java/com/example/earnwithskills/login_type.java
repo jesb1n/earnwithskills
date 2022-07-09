546d08d70fcea0495dd8b240022abc92138565fb
Gokul P V <dwarakagokul@gmail.com>
2022-07-09 18:21:45 +0530

    commit 3

--- app/src/main/java/com/example/earnwithskills/login_type.java
+++ app/src/main/java/com/example/earnwithskills/login_type.java
@@ -2,20 +2,35 @@ package com.example.earnwithskills;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.earnwithskills.databinding.ActivityLoginTypeBinding;
+
 public class login_type extends AppCompatActivity {
 
+    ActivityLoginTypeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_type);
+        binding = ActivityLoginTypeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.activity_login_type);
+
     }
 
     public void hirer(View view) {
+        String x="hirer";
+        Intent i = new Intent(login_type.this, login.class);
+        i.putExtra("key",x);
+        startActivity(i);
     }
 
     public void student(View view) {
+        String x="student";
+        Intent i = new Intent(login_type.this, login.class);
+        i.putExtra("key",x);
+        startActivity(i);
     }
 }

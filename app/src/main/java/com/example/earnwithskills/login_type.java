c89dc3a5adc4ec9633dc6dfe40e38b352b4c9afd
Gokul P V <dwarakagokul@gmail.com>
2022-07-09 17:17:54 +0530

    commit2

---
+++ app/src/main/java/com/example/earnwithskills/login_type.java
@@ -0,0 +1,21 @@
+package com.example.earnwithskills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class login_type extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_type);
+    }
+
+    public void hirer(View view) {
+    }
+
+    public void student(View view) {
+    }
+}

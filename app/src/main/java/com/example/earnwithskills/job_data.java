00aa8aa2c0ec3020e1d3d60a5c3814e671de0c5e
Jowin <jowingeorge2015@gmail.com>
2022-07-10 04:31:10 +0530

    hire

---
+++ app/src/main/java/com/example/earnwithskills/job_data.java
@@ -0,0 +1,12 @@
+package com.example.earnwithskills;
+
+public class job_data {
+    private String job_name;
+    private String job_skill;
+
+    public job_data(String job_name, String job_skill) {
+        this.job_name = job_name;
+        this.job_skill = job_skill;
+    }
+
+}
